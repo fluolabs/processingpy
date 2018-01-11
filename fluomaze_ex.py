7cecbd1a2193aa8ed20ef633e23f3d897d024a17
fluolabs <34490613+fluolabs@users.noreply.github.com>
2018-01-10 22:25:25 -0600

    Update fluomaze_ex.py

--- fluomaze_ex.py
+++ fluomaze_ex.py
@@ -2,17 +2,25 @@ import fluo
 
 def setup():
     size(400, 400)
-    fluo.showGrid(grid=True)
+    fluo.showGrid(index=True)
     fluo.setSprite(7, 12)
-    fluo.setFinalTarget(13, 12)
+    
+    fluo.xyToGrid(PVector(20, 20))
+    fluo.setDestination(13, 12)
     fluo.addRoad(7, 12, 13, 12)
-    #fluo.drawRoads()
-    #fluo.drawSprite()
-    #fluo.drawFinalTarget()
+    fluo.drawRoads()
+    fluo.drawSprite()
+    fluo.drawDestination()
+    fluo.printSprite()
+    fluo.setTarget(7, 13)
+    fluo.printTarget()
     
 def draw():
-    fluo.moveForward()
-    fluo.moveForward()
+    pass
+    #fluo.printSprite()
+    #fluo.display()
+    #fluo.moveForward()
+    #fluo.moveForward()
     #pass
     #background(220)
 
@@ -23,3 +31,4 @@ def draw():
     #fluo.setSprite(7, 12)
     #fluo.drawSprite()
     #fluo.drawTarget(13, 12)
+    

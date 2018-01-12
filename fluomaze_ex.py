5a2c331877c2314173106c9c1374249889941be8
fluolabs <34490613+fluolabs@users.noreply.github.com>
2018-01-11 21:46:43 -0600

    Update fluomaze_ex.py

--- fluomaze_ex.py
+++ fluomaze_ex.py
@@ -4,36 +4,31 @@ def setup():
     size(400, 400)
     fluo.showGrid(index=True)
     fluo.setSprite(7, 12)
-    
-    fluo.xyToGrid(PVector(20, 20))
+    #fluo.xyToGrid(PVector(20, 20))
     fluo.setDestination(13, 12)
     fluo.addRoad(7, 12, 13, 12)
+    fluo.addRoad(13, 12, 13, 7)
     fluo.drawRoads()
     fluo.drawSprite()
     fluo.drawDestination()
     #fluo.printSprite()
     #fluo.setTarget(8, 12)
     #fluo.printTarget()
-    fluo.turnRight()
+    fluo.moveForward()
+    #fluo.moveForward()
+    #fluo.turnLeft()
+    #fluo.moveForward()
+    #fluo.moveForward()
+    
+    #fluo.turnLeft()
     #fluo.commands = [0, 1]
+    #fluo.moveForward()
+    #fluo.moveBackward()
+    #fluo.moveForward()
     #fluo.test()
     
 def draw():
-    
-    #fluo.printSprite()
     fluo.display()
-    #fluo.moveForward()
-    #fluo.moveForward()
-    #pass
-    #background(220)
-
-    #fluo.showGrid(grid=True)
-    #print(fluo.nRows, fluo.nCols)
-    
-    #fluo.drawRoad(7, 12, 13, 12)
-    #fluo.setSprite(7, 12)
-    #fluo.drawSprite()
-    #fluo.drawTarget(13, 12)
     
     
 def keyPressed():
